de45608d334249f8f8abb6ed6da6afe28cd474de
Danil Pismenny <danil@orionet.ru>
2015-02-20 22:54:54 +0300

    sortable helper

--- app/assets/javascripts/activeadmin-sortable.js
+++ app/assets/javascripts/activeadmin-sortable.js
@@ -5,15 +5,28 @@
 
   $.fn.activeAdminSortable = function() {
     this.sortable({
+      helper: function (e, tr) {
+          var originals = tr.children();
+          var helper = tr.clone();
+          helper.children().each(function (index) {
+              $(this).width(originals.eq(index).width());
+          });
+          return helper;
+      },
+      handle: ".handle",
+      axis: "y",
+      placeholder: "ui-state-highlight",
       update: function(event, ui) {
         var item = ui.item.find('[data-sort-url]')
-        var url = item.data('sort-url');
+        var url  = item.data('sort-url');
         var actionOnSuccess = item.data('sort-success-action')
+        var position = item.data('data-sort-position') || (ui.item.index() + 1);
+        var method   = item.data('data-sort-post') || 'post'
 
         $.ajax({
-          url: url,
-          type: 'post',
-          data: { position: ui.item.index() + 1 },
+          url:  url,
+          type: method,
+          data: { position: position },
           error: function() { alert("Saving sortable error"); },
           success: function() { if (actionOnSuccess=='nothing') { return; } else { window.location.reload(); } }
         });
